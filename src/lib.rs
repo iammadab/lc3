0b07f389bf50322a243644dd2d43aa9d50f12f82
Wisdom Ogwu <iammadab@gmail.com>
2024-11-19 13:15:09 +0100

    test add opcode

--- src/lib.rs
+++ src/lib.rs
@@ -166,7 +166,7 @@ fn add(vm: &mut VM, instruction: u16) {
 
 #[cfg(test)]
 mod tests {
-    use crate::{sext, Opcode, Register, VM};
+    use crate::{add, sext, Opcode, Register, VM};
 
     // (instr_value, instr_bit_count)
     type InstructionPart = (u16, u8);
@@ -192,6 +192,10 @@ mod tests {
 
     fn encode_opcode(opcode: Opcode) -> InstructionPart {
         (opcode.into(), OPCODE_BIT_COUNT)
+    }
+
+    fn encode_imm5(val: u16) -> InstructionPart {
+        (val, 5)
     }
 
     #[test]
@@ -238,5 +242,39 @@ mod tests {
             ]),
             0b0001_000_001_0_00_010
         );
+    }
+
+    #[test]
+    fn test_add_opcode() {
+        // init vm
+        let mut vm = VM::init();
+        *vm.reg_mut(Register::R3.into()) = 4;
+        *vm.reg_mut(Register::R4.into()) = 5;
+
+        assert_eq!(vm.reg(Register::R2.into()), 0);
+
+        // ADD R2, R3, R4 <- R2 = R3 + R4
+        let instr_1 = encode_instruction(vec![
+            encode_opcode(Opcode::ADD),
+            encode_register(Register::R2),
+            encode_register(Register::R3),
+            (0, 1),
+            (0, 2),
+            encode_register(Register::R4),
+        ]);
+
+        // ADD R2, R3, 7  <- R2 = R3 + 7
+        let instr_2 = encode_instruction(vec![
+            encode_opcode(Opcode::ADD),
+            encode_register(Register::R2),
+            encode_register(Register::R3),
+            (1, 1),
+            encode_imm5(7),
+        ]);
+
+        add(&mut vm, instr_1);
+        assert_eq!(vm.reg(Register::R2.into()), 9);
+        add(&mut vm, instr_2);
+        assert_eq!(vm.reg(Register::R2.into()), 11);
     }
 }
