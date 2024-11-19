336b543f647c1ff2a66d7f6091a8a4c49f6e4b2f
Wisdom Ogwu <iammadab@gmail.com>
2024-11-19 08:34:37 +0100

    test register implicit ordering

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+enum Registers {
+    R0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    PC,
+    COND,
+    COUNT
+}
+struct VM {
+    memory: [u16; 1 << 16],
+    registers: [u16; 10]
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::Registers;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_register_implicit_ordering() {
+        assert_eq!(Registers::R0 as usize, 0);
+        assert_eq!(Registers::R3 as usize, 3);
+        assert_eq!(Registers::PC as usize, 8);
     }
 }
