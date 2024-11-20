fa7812ec4b429264b102d525dc060b5c9c93b058
Wisdom Ogwu <iammadab@gmail.com>
2024-11-20 16:11:58 +0100

    termios

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,26 @@ use std::fs::File;
 use std::io;
 use std::io::ErrorKind::UnexpectedEof;
 use std::io::{BufReader, Read};
+use termios::*;
 
 pub mod decode_instruction;
 pub mod opcodes;
 pub mod vm;
 
 fn main() {
-    let path = "./src/programs/rogue.obj";
+    // Some tricks to make the VM's terminal be interactive
+    let stdin = 0;
+    let termios = termios::Termios::from_fd(stdin).unwrap();
+
+    // make a mutable copy of termios
+    // that we will modify
+    let mut new_termios = termios.clone();
+    new_termios.c_iflag &= IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON;
+    new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
+
+    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+
+    let path = "./src/programs/2048.obj";
     let f = File::open(path).unwrap();
     let mut f = BufReader::new(f);
 
@@ -35,6 +48,10 @@ fn main() {
     }
 
     vm.run();
+
+    // reset the stdin to
+    // original termios data
+    tcsetattr(stdin, TCSANOW, &termios).unwrap();
 }
 
 fn read_u16(f: &mut BufReader<File>) -> io::Result<u16> {
